52525fa8b48d1175ab45db13550157f3a0625da8
CameronPersonett <CameronPersonett@users.noreply.github.com>
2021-07-21 11:30:18 -0400

    Add files via upload

---
+++ Midi2Lua/NoteEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MidiToLua {
+    public class NoteEvent {
+        public string note;
+        public int position;
+        public string instrument;
+
+        public NoteEvent(int track, string note, int position) {
+            this.note = note;
+            this.position = position;
+            instrument = Instruments.GetInstrument(track, note);
+        }
+
+        public override string ToString() {
+            return note + ", " + position + ", " + instrument;
+        }
+    }
+}
---
+++ Midi2Lua/Song.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MidiToLua {
+    public class Song {
+        public string name;
+        public int totalSamples;
+
+        public List<Section> sections;
+        public List<NoteEvent> noteEvents;
+
+        public Song(string name) {
+            this.name = name;
+            totalSamples = 0;
+            sections = new List<Section>();
+            noteEvents = new List<NoteEvent>();
+        }
+
+        public void AddSection(Section section) {
+            sections.Add(section);
+            totalSamples += section.sampleEnd - section.sampleBegin;
+        }
+
+        public void AddNoteEvent(NoteEvent noteEvent) {
+            noteEvents.Add(noteEvent);
+        }
+    }
+}
