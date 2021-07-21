9d8c092f1a9d8f7636db73209e6186632f5828c6
CameronPersonett <camerongdp@gmail.com>
2021-07-21 16:31:52 -0400

    * NoteEvent.cs: * Instruments.cs:
    * Program.cs: Further optimized script building.
    This results in up to an 800%+ decrease in file size.

--- Midi2Lua/NoteEvent.cs
+++ Midi2Lua/NoteEvent.cs
@@ -16,5 +16,185 @@ namespace MidiToLua {
         public override string ToString() {
             return note + ", " + position + ", " + instrument;
         }
+
+        public string GetNoteVar() {
+            if(note.Equals("F#1")) {
+                return "aa";
+            } else if(note.Equals("G1")) {
+                return "ab";
+            } else if(note.Equals("G#1")) {
+                return "ac";
+            } else if(note.Equals("A1")) {
+                return "ad";
+            } else if(note.Equals("A#1")) {
+                return "ae";
+            } else if(note.Equals("B1")) {
+                return "af";
+            } else if(note.Equals("C2")) {
+                return "ag";
+            } else if(note.Equals("C#2")) {
+                return "ah";
+            } else if(note.Equals("D2")) {
+                return "ai";
+            } else if(note.Equals("D#2")) {
+                return "aj";
+            } else if(note.Equals("E2")) {
+                return "ak";
+            } else if(note.Equals("F2")) {
+                return "al";
+            } else if(note.Equals("F#2")) {
+                return "am";
+            } else if(note.Equals("G2")) {
+                return "an";
+            } else if(note.Equals("G#2")) {
+                return "ao";
+            } else if(note.Equals("A2")) {
+                return "ap";
+            } else if(note.Equals("A#2")) {
+                return "aq";
+            } else if(note.Equals("B2")) {
+                return "ar";
+            } else if(note.Equals("C3")) {
+                return "as";
+            } else if(note.Equals("C#3")) {
+                return "at";
+            } else if(note.Equals("D3")) {
+                return "au";
+            } else if(note.Equals("D#3")) {
+                return "av";
+            } else if(note.Equals("E3")) {
+                return "aw";
+            } else if(note.Equals("F3")) {
+                return "ax";
+            } else if(note.Equals("F#3")) {
+                return "ay";
+            } else if(note.Equals("G3")) {
+                return "az";
+            } else if(note.Equals("G#3")) {
+                return "ba";
+            } else if(note.Equals("A3")) {
+                return "bb";
+            } else if(note.Equals("A#3")) {
+                return "bc";
+            } else if(note.Equals("B3")) {
+                return "bd";
+            } else if(note.Equals("C4")) {
+                return "be";
+            } else if(note.Equals("C#4")) {
+                return "bf";
+            } else if(note.Equals("D4")) {
+                return "bg";
+            } else if(note.Equals("D#4")) {
+                return "bh";
+            } else if(note.Equals("E4")) {
+                return "bi";
+            } else if(note.Equals("F4")) {
+                return "bj";
+            } else if(note.Equals("F#1")) {
+                return "bk";
+            } else if(note.Equals("G4")) {
+                return "bl";
+            } else if(note.Equals("G#4")) {
+                return "bm";
+            } else if(note.Equals("A4")) {
+                return "bn";
+            } else if(note.Equals("A#4")) {
+                return "bo";
+            } else if(note.Equals("B4")) {
+                return "bp";
+            } else if(note.Equals("C5")) {
+                return "bq";
+            } else if(note.Equals("C#5")) {
+                return "br";
+            } else if(note.Equals("D5")) {
+                return "bs";
+            } else if(note.Equals("D#5")) {
+                return "bt";
+            } else if(note.Equals("E5")) {
+                return "bu";
+            } else if(note.Equals("F5")) {
+                return "bv";
+            } else if(note.Equals("F#5")) {
+                return "bw";
+            } else if(note.Equals("G5")) {
+                return "bx";
+            } else if(note.Equals("G#5")) {
+                return "by";
+            } else if(note.Equals("A5")) {
+                return "bz";
+            } else if(note.Equals("A#5")) {
+                return "ca";
+            } else if(note.Equals("B5")) {
+                return "cb";
+            } else if(note.Equals("C6")) {
+                return "cc";
+            } else if(note.Equals("C#6")) {
+                return "cd";
+            } else if(note.Equals("D6")) {
+                return "ce";
+            } else if(note.Equals("D#6")) {
+                return "cf";
+            } else if(note.Equals("E6")) {
+                return "cg";
+            } else if(note.Equals("F6")) {
+                return "ch";
+            } else if(note.Equals("F#6")) {
+                return "ci";
+            } else if(note.Equals("G6")) {
+                return "cj";
+            } else if(note.Equals("G#6")) {
+                return "ck";
+            } else if(note.Equals("A6")) {
+                return "cl";
+            } else if(note.Equals("A#6")) {
+                return "cm";
+            } else if(note.Equals("B6")) {
+                return "cn";
+            } else if(note.Equals("C7")) {
+                return "co";
+            } else if(note.Equals("C#7")) {
+                return "cp";
+            } else if(note.Equals("D7")) {
+                return "cq";
+            } else if(note.Equals("D#7")) {
+                return "cr";
+            } else if(note.Equals("E7")) {
+                return "cs";
+            } else if(note.Equals("F7")) {
+                return "ct";
+            } else if(note.Equals("F#7")) {
+                return "cu";
+            } return "u";
+        }
+
+        public string GetInstrumentVar() {
+            if(instrument.Equals("bass")) {
+                return "b";
+            } else if(instrument.Equals("guitar")) {
+                return "g";
+            } else if(instrument.Equals("flute")) {
+                return "f";
+            } else if(instrument.Equals("bell")) {
+                return "e";
+            } else if(instrument.Equals("bassDrum")) {
+                return "a";
+            } else if(instrument.Equals("lowFloorTom")) {
+                return "l";
+            } else if(instrument.Equals("highFloorTom")) {
+                return "h";
+            } else if(instrument.Equals("lowTom")) {
+                return "o";
+            } else if(instrument.Equals("lowMidTom")) {
+                return "m";
+            } else if(instrument.Equals("highMidTom")) {
+                return "d";
+            } else if(instrument.Equals("highTom")) {
+                return "i";
+            } else if(instrument.Equals("snare")) {
+                return "s";
+            } else if(instrument.Equals("hat")) {
+                return "t";
+            } return "u";
+        }
     }
 }
--- Midi2Lua/Program.cs
+++ Midi2Lua/Program.cs
@@ -111,8 +111,99 @@ namespace MidiToLua {
 
             lua.Add("function add(sample, note, instrument)");
             lua.Add("    table.insert(song.samples[sample].noteEvents, " +
-                "{ note = note, instrument = instrument }");
+                "{ note = note, instrument = instrument })");
             lua.Add("end");
+            lua.Add("");
+
+            lua.Add("b='bass'");
+            lua.Add("g='guitar'");
+            lua.Add("f='flute'");
+            lua.Add("e='bell'");
+            lua.Add("a='bassDrum'");
+            lua.Add("l='lowFloorTom'");
+            lua.Add("h='highFloorTom'");
+            lua.Add("o='lowTom'");
+            lua.Add("m='lowMidTom'");
+            lua.Add("d='highMidTom'");
+            lua.Add("i='highTom'");
+            lua.Add("s='snare'");
+            lua.Add("t='hat'");
+            lua.Add("u='unkown'");
+            lua.Add("");
+
+            lua.Add("aa='F#1'");
+            lua.Add("ab='G1'");
+            lua.Add("ac='G#1'");
+            lua.Add("ad='A1'");
+            lua.Add("ae='A#1'");
+            lua.Add("af='B1'");
+            lua.Add("ag='C2'");
+            lua.Add("ah='C#2'");
+            lua.Add("ai='D2'");
+            lua.Add("aj='D#2'");
+            lua.Add("ak='E2'");
+            lua.Add("al='F2'");
+            lua.Add("am='F#2'");
+            lua.Add("an='G2'");
+            lua.Add("ao='G#2'");
+            lua.Add("ap='A2'");
+            lua.Add("aq='A#2'");
+            lua.Add("ar='B2'");
+            lua.Add("as='C3'");
+            lua.Add("at='C#3'");
+            lua.Add("au='D3'");
+            lua.Add("av='D#3'");
+            lua.Add("aw='E3'");
+            lua.Add("ax='F3'");
+            lua.Add("ay='F#3'");
+            lua.Add("az='G3'");
+            lua.Add("ba='G#3'");
+            lua.Add("bb='A3'");
+            lua.Add("bc='A#3'");
+            lua.Add("bd='B3'");
+            lua.Add("be='C4'");
+            lua.Add("bf='C#4'");
+            lua.Add("bg='D4'");
+            lua.Add("bh='D#4'");
+            lua.Add("bi='E4'");
+            lua.Add("bj='F4'");
+            lua.Add("bk='F#4'");
+            lua.Add("bl='G4'");
+            lua.Add("bm='G#4'");
+            lua.Add("bn='A4'");
+            lua.Add("bo='A#4'");
+            lua.Add("bp='B4'");
+            lua.Add("bq='C5'");
+            lua.Add("br='C#5'");
+            lua.Add("bs='D5'");
+            lua.Add("bt='D#5'");
+            lua.Add("bu='E5'");
+            lua.Add("bv='F5'");
+            lua.Add("bw='F#5'");
+            lua.Add("bx='G5'");
+            lua.Add("by='G#5'");
+            lua.Add("bz='A5'");
+            lua.Add("ca='A#5'");
+            lua.Add("cb='B5'");
+            lua.Add("cc='C6'");
+            lua.Add("cd='C#6'");
+            lua.Add("ce='D6'");
+            lua.Add("cf='D#6'");
+            lua.Add("cg='E6'");
+            lua.Add("ch='F6'");
+            lua.Add("ci='F#6'");
+            lua.Add("cj='G6'");
+            lua.Add("ck='G#6'");
+            lua.Add("cl='A6'");
+            lua.Add("cm='A#6'");
+            lua.Add("cn='B6'");
+            lua.Add("co='C7'");
+            lua.Add("cp='C#7'");
+            lua.Add("cq='D7'");
+            lua.Add("cr='D#7'");
+            lua.Add("cs='E7'");
+            lua.Add("ct='F7'");
+            lua.Add("cu='F#7'");
             lua.Add("");
 
             lua.Add("song = {}");
@@ -127,8 +218,9 @@ namespace MidiToLua {
             lua.Add("");
 
             for(int i = 0; i < song.noteEvents.Count; i++) {
-                lua.Add("add(" + (GetSampleNumber(song.noteEvents[i].position) + 1) + ", '" +
-                    song.noteEvents[i].note + "', '" + song.noteEvents[i].instrument + "')");
+                lua.Add("add(" + (GetSampleNumber(song.noteEvents[i].position) + 1) + ", " +
+                    song.noteEvents[i].GetNoteVar() + ", " +
+                    song.noteEvents[i].GetInstrumentVar() + ")");
             }
         }
 
